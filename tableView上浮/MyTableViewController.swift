63d5d0dfd33d55912fea64f6f6cecf37b218eb8a
wangfei <785509893@qq.com>
2018-08-28 11:58:57 +0800

    提交代码

---
+++ tableView上浮/MyTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  MyTableViewController.swift
+//  tableView上浮
+//
+//  Created by 王飞 on 2018/7/26.
+//  Copyright © 2018年 农业大脑. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var cell2: UITableViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+//        cell2.layer.cornerRadius = 15
+//        cell2.layer.masksToBounds = true
+//        self.tableView.bringSubview(toFront: cell2)
+        cell2.layer.zPosition = 100
+        
+        // 加半圆角
+        var maskPath:UIBezierPath!
+        maskPath = UIBezierPath.init(roundedRect: button.bounds, byRoundingCorners: UIRectCorner(rawValue: UIRectCorner.RawValue(UInt8(UIRectCorner.topRight.rawValue) | UInt8(UIRectCorner.topLeft.rawValue))), cornerRadii: CGSize(width:6,height:6))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = button.bounds
+        maskLayer.path = maskPath.cgPath
+        button.layer.mask = maskLayer;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        if indexPath.row == 1 {
+//            return 60
+//        }
+        
+        return 44
+    }
+
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+//
+//        // Configure the cell...
+//        if cell == cell2 {
+//            self.tableView.bringSubview(toFront: cell2)
+//        }
+//
+//        return cell
+//    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
